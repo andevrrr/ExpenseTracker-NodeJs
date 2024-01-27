dd0b4b6565668e35184cc158ec14a09b7be051bd
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-28 01:00:26 +0200

    change return to res.json

--- controllers/calls.js
+++ controllers/calls.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 exports.getData = (req, res) => {
-    fs.readFile("./data.json", function(err, data) {
-        if (err) throw err;
-        const bankStatement = JSON.parse(data);
-        console.log(bankStatement);
-        return data;
-    })
-}
+  fs.readFile("./controllers/data.json", function (err, data) {
+    if (err) throw err;
+    const bankStatement = JSON.parse(data);
+    //console.log(bankStatement);
+    res.json(bankStatement);
+  });
+};
