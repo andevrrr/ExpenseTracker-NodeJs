2f9c7cee3eb267cc003ff415047c21121d7f9c92
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-28 23:39:23 +0200

    added update api endpoint

--- controllers/calls.js
+++ controllers/calls.js
@@ -1,8 +1,30 @@
 exports.getData = (req, res) => {
+  console.log(req.session.transactions);
   if (req.session.transactions) {
     res.json(req.session.transactions);
   } else {
     console.log("not available");
     res.status(404).send("No data available");
+  }
+};
+
+exports.updateCategory = (req, res) => {
+  const { paymentDate, businessName, newCategory } = req.body;
+
+  if (req.session.transactions) {
+    const transactions = req.session.transactions.map((transaction) => {
+      if (
+        transaction.paymentDate === paymentDate &&
+        transaction.businessName === businessName
+      ) {
+        return { ...transaction, category: newCategory };
+      }
+      return transaction;
+    });
+
+    req.session.transactions = transactions;
+    res.json({ message: "Category updated successfully." });
+  } else {
+    res.status(404).send("Session or transaction not found.");
   }
 };
