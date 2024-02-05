66bfe6501ef44e53c06c10881bab231223f9fe31
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-02-05 11:12:46 +0200

    refactored for better perfomance

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,136 +1,81 @@
 const fs = require("fs");
 const csv = require("csv-parser");
+const util = require("util");
+const unlinkAsync = util.promisify(fs.unlink);
 const categorizeTransaction = require("../scripts/script.js");
+const { income, outcome } = require("./categories.js");
 
-exports.postUploadFile = (req, res) => {
-  const filePath = req.file.path;
-  req.session.filePath = filePath;
-  const outcome = [
-    "Transportation",
-    "Subscriptions",
-    "Memberships",
-    "Housing",
-    "Transfers",
-    "Groceries", // Ruokakaupat
-    "Utilities", // Käyttömenot
-    "Dining", // Ruokailu
-    "Entertainment", // Viihde
-    "Travel", // Matkustus
-    "Healthcare", // Terveydenhuolto
-    "Fashion", // Muoti
-    "Recreation", // Vapaa-aika
-    "Electronics", // Teknologia
-    "Homeware", // Kotitavarat
-    "Education", // Koulutus
-    "Finance", // Rahoitus
-    "Pets", // Lemmikit
-    "Hobbies", // Harrastukset
-    "E-Commerce", // Verkkokauppa
-    "Services", // Palvelut
-    "Eco", // Ekologisuus
-    "Taxi", // Taksi
-    "Bars", // Baarit
-    "Clubs", // Klubit
-    "Fitness", // Kuntoilu
-    "Beauty", // Kauneus
-    "Books", // Kirjat
-    "Games", // Pelit
-    "Gifts", // Lahjat
-    "Music", // Musiikki
-    "Sports", // Urheilu
-    "Outdoor", // Ulkoilu
-    "Other",
-  ];
+exports.postUploadFile = async (req, res) => {
+  if (!req.file || !req.file.path) {
+    return res.status(400).send("No file uploaded or file path missing.");
+  }
 
-  const income = [
-    "Salary",
-    "Deposits",
-    "Transfers",
-    "MobilePay",
-    "Interest",
-    "Dividends",
-    "Gifts",
-    "Rental Income",
-    "Other",
-  ];
+  try {
+    const filePath = req.file.path;
+    req.session.filePath = filePath;
 
-  const incomeTransactions = [];
-  const outcomeTransactions = [];
-  const transactions = [];
+    const transactions = await parseCSV(filePath);
+    const categorizedTransactions = await categorizeTransactions(transactions);
 
-  fs.createReadStream(filePath)
-    .pipe(
-      csv({
-        separator: ";",
-        headers: [
-          "Kirjauspäivä",
-          "Maksupäivä",
-          "Summa",
-          "Tapahtumalaji",
-          "Maksaja",
-          "Saajan_nimi",
-        ],
-      })
-    )
-    .on("data", (data) => {
-      // Format paymentDate to YYYY-MM-DD
-      let formattedDate = data.Maksupäivä.split(".").reverse().join("-");
+    req.session.transactions = categorizeByType(categorizedTransactions);
 
-      // Convert amount to use period as decimal separator and adjust the sign if necessary
-      let formattedAmount = data.Summa.replace(",", ".");
-
-      const transaction = {
-        paymentDate: formattedDate,
-        businessName: data.Saajan_nimi,
-        payer: data.Maksaja,
-        amount: formattedAmount,
-        transactionType: data.Tapahtumalaji,
-      };
-      transactions.push(transaction);
-    })
-    .on("end", async () => {
-      let transactionId = 0;
-      const categorizedTransactions = [];
-      for (let transaction of transactions) {
-        transaction.id = ++transactionId;
-
-        if (parseFloat(transaction.amount) > 0) {
-          // Categorize positive amount as income
-          transaction.category = await categorizeTransaction(
-            transaction,
-            income
-          );
-          incomeTransactions.push(transaction);
-        } else {
-          // Categorize negative amount as outcome
-          transaction.category = await categorizeTransaction(
-            transaction,
-            outcome
-          );
-          outcomeTransactions.push(transaction);
-        }
-
-        categorizedTransactions.push(transaction);
-      }
-
-      const combinedCategories = {
-        incomeCategories: Array.from(incomeTransactions),
-        outcomeCategories: Array.from(outcomeTransactions),
-      };
-
-      console.log(combinedCategories);
-
-      req.session.transactions = combinedCategories;
-
-      req.session.save((err) => {
-        if (err) {
-          console.error("Session save error:", err);
-          return res.status(500).send("Internal server error");
-        }
-        console.log("Session ID:", req.sessionID);
-
-        console.log("Categorized transactions stored in session.");
-        res.json(categorizedTransactions);
-      });
-    });
+    await req.session.save();
+    res.json(categorizedTransactions);
+  } catch (error) {
+    console.error("Error processing file:", error);
+    res.status(500).send("Error processing file");
+  } finally {
+    await unlinkAsync(req.file.path).catch(console.error);
+  }
 };
+
+const parseCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    let transactionId = 0;
+    const transactions = [];
+    fs.createReadStream(filePath)
+      .pipe(
+        csv({
+          separator: ";",
+          headers: [
+            "Kirjauspäivä",
+            "Maksupäivä",
+            "Summa",
+            "Tapahtumalaji",
+            "Maksaja",
+            "Saajan_nimi",
+          ],
+        })
+      )
+      .on("data", (data) => {
+        transactionId++;
+        transactions.push(formatTransaction(data, transactionId));
+      })
+      .on("end", () => resolve(transactions))
+      .on("error", reject);
+  });
+};
+
+const formatTransaction = (data, id) => ({
+  id,
+  paymentDate: data.Maksupäivä.split(".").reverse().join("-"),
+  businessName: data.Saajan_nimi,
+  payer: data.Maksaja,
+  amount: data.Summa.replace(",", "."),
+  transactionType: data.Tapahtumalaji,
+});
+
+const categorizeTransactions = async (transactions) => {
+  for (const transaction of transactions) {
+    transaction.category = await categorizeTransaction(
+      transaction,
+      parseFloat(transaction.amount) > 0 ? income : outcome
+    );
+  }
+  return transactions;
+};
+
+const categorizeByType = (transactions) => ({
+  incomeCategories: transactions.filter((t) => parseFloat(t.amount) > 0),
+  outcomeCategories: transactions.filter((t) => parseFloat(t.amount) <= 0),
+});
